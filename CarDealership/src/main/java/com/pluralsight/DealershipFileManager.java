7182c0d9dc11828554713ca5cad20117c95fbedb
Brenda Vang <vangsbrenda@gmail.com>
2025-05-09 15:37:01 -0500

    Adding to inventory.csv

--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -2,6 +2,14 @@ package com.pluralsight;
 
 public class DealershipFileManager {
 
+    // creating the file path for loading and reading data
+    private static final String filePath = "src/main/resources/inventory.csv";
+
+    // creating getDealership method
+    public Dealership getDealership() {
+
+
+    }
 
 
 }
