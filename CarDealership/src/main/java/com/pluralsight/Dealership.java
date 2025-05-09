75e85fc16d86bcd9d53ef68e778992985c07f280
Brenda Vang <vangsbrenda@gmail.com>
2025-05-09 14:23:54 -0500

    Re-arranging methods, adding methods

--- CarDealership/src/main/java/com/pluralsight/Dealership.java
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Dealership {
 
-
     private String name;
     private String address;
     private String phone;
@@ -20,13 +19,50 @@ public class Dealership {
         this.inventory = new ArrayList<Vehicle>();
     }
 
-    // creating addVehicle method, adds vehicle to list
-    public void addVehicle() {
+
+    // get vehicles by price (min & max)
+    public ArrayList<Vehicle> getVehiclesByPrice() {
+
+    }
+
+    // get vehicles by make & model
+    public ArrayList<Vehicle> getVehiclesByMakeModel() {
+
+    }
+
+    // get vehicles by the year (min & max)
+    public ArrayList<Vehicle> getVehiclesByYear() {
+
+    }
+
+    // get vehicles by color
+    public ArrayList<Vehicle> getVehiclesByColor() {
+
+    }
+
+
+    // get vehicles by mileage (min & max)
+    public ArrayList<Vehicle> getVehiclesByMileage() {
+
+    }
+
+    // car, truck, SUV, van
+    public ArrayList<Vehicle> getVehiclesByType() {
 
     }
 
     // creating getter for getAllVehicles
     public ArrayList<Vehicle> getAllVehicles() {
         return inventory();
+    }
+
+    // creating addVehicle method, adds vehicle to list
+    public void addVehicle() {
+
+    }
+
+    // removes vehicle
+    public void removeVehicle() {
+
     }
 }
