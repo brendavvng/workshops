3aebf138f2ecd1b3d592237ef00ddce5918c0369
Brenda Vang <vangsbrenda@gmail.com>
2025-05-09 14:25:58 -0500

    Working on Vehicle class

--- CarDealership/src/main/java/com/pluralsight/Vehicle.java
+++ CarDealership/src/main/java/com/pluralsight/Vehicle.java
@@ -2,5 +2,17 @@ package com.pluralsight;
 
 public class Vehicle {
 
+    // creating variables
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+
+
     // getters and setters
 }
